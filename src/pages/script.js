608e2238662779851b5c0abdac3063d65ce626dd
Caue Rodrigues Reis <caue.reis@APRS.EDU.BR>
2025-12-02 17:34:43 -0300

    feat: added main sections

--- src/pages/script.js
+++
@@ -1,8 +0,0 @@
-// Scroll suave
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-    link.addEventListener("click", function(e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute("href"));
-        target.scrollIntoView({ behavior: "smooth" });
-    });
-});
